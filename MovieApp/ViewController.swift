bb3a5db494a86252bcf320d99d919eb29017d7a4
KahPiloupas <karinapiloupas@gmail.com>
2024-11-08 10:58:31 -0300

    Commiting splashscreen

---
+++ MovieApp/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  MovieApp
+//
+//  Created by Karina Piloupas Da Costa on 08/11/24.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        view.backgroundColor = .red
+    }
+
+
+}
+
