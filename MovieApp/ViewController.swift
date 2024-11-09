8dc8633c896e5bca491cfd3c29dea6b541cc82d2
KahPiloupas <karinapiloupas@gmail.com>
2024-11-09 16:11:29 -0300

    Implementing splashscreen

---
+++ MovieApp/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  MovieApp
+//
+//  Created by Karina Piloupas Da Costa on 09/11/24.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        self.view.backgroundColor = .red
+    }
+
+
+}
+
